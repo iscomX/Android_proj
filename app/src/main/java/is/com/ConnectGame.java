a0a337ea2fb3ab50d7e04f3a717fb3794803b322
iscom git <iscom321@gmail.com>
2018-02-08 13:59:47 +0000

    05- P3 Connect Game end

--- app/src/main/java/is/com/ConnectGame.java
+++ app/src/main/java/is/com/ConnectGame.java
@@ -13,91 +13,58 @@ import com.example.iscom.sharedprefs.R;
 
 public class ConnectGame extends ActionBarActivity {
 
-    // 0 = yellow, 1 = red
+    // 0 = white_rond, 1 = black_rond
 
     int activePlayer = 0;
 
     boolean gameIsActive = true;
 
-    // 2 means unplayed
+    // 7 means unplayed
 
-    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[] gameState = {7, 7, 7, 7, 7, 7, 7, 7, 7};
 
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
     public void playCennectGame(View view) {
 
         ImageView counter = (ImageView) view;
-
-
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (gameState[tappedCounter] == 2 && gameIsActive) {
-
+        if (gameState[tappedCounter] == 7 && gameIsActive) {
             gameState[tappedCounter] = activePlayer;
-
             counter.setTranslationY(-1000f);
-
             if (activePlayer == 0) {
-
-                counter.setImageResource(R.drawable.yellow);
-
+                counter.setImageResource(R.drawable.white_rond);
                 activePlayer = 1;
-
             } else {
-
-                counter.setImageResource(R.drawable.red);
-
+                counter.setImageResource(R.drawable.black_rond);
                 activePlayer = 0;
-
             }
 
             counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
-
             for (int[] winningPosition : winningPositions) {
-
                 if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
                         gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
-                        gameState[winningPosition[0]] != 2) {
-
+                        gameState[winningPosition[0]] != 7) {
                     // Someone has won!
-
                     gameIsActive = false;
-
-                    String winner = "Red";
-
+                    String winner = "Black";
                     if (gameState[winningPosition[0]] == 0) {
-
-                        winner = "Yellow";
-
+                        winner = "White";
                     }
-
-                    TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
-
-                    winnerMessage.setText(winner + " has won!");
-
+                    TextView Message = (TextView) findViewById(R.id.winnerMessage);
+                    Message.setText(winner + " is win");
                     LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
-
                     layout.setVisibility(View.VISIBLE);
-
                 } else {
-
                     boolean gameIsOver = true;
-
                     for (int counterState : gameState) {
-
-                        if (counterState == 2) gameIsOver = false;
-
+                        if (counterState == 7) gameIsOver = false;
                     }
-
                     if (gameIsOver) {
-
                         TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
-
                         winnerMessage.setText("It's a draw");
-
                         LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
-
                         layout.setVisibility(View.VISIBLE);
 
                     }
@@ -111,27 +78,17 @@ public class ConnectGame extends ActionBarActivity {
     }
 
     public void playAgain(View view) {
-
         gameIsActive = true;
-
         LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
-
         layout.setVisibility(View.INVISIBLE);
-
         activePlayer = 0;
-
         for (int i = 0; i < gameState.length; i++) {
-
-            gameState[i] = 2;
-
+            gameState[i] = 7;
         }
 
         GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
-
         for (int i = 0; i < gridLayout.getChildCount(); i++) {
-
             ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
-
         }
 
     }
